0acee310373944d4d65fe8368cd5e9abea7631b2
Aamina Siraj <aaminasiraj23@gmail.com>
2026-07-31 20:31:27 +0530

    general c basics

---
+++ Basics/calculator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+
+    float x, y ; 
+
+    printf("Enter 2 values to be added: \n");
+    scanf("%f , %f", &x, &y);
+    
+    printf("The sum of %f and %f is %f. \n", x, y, x + y);
+
+
+}
