ab129c00676ef9598d4cb682b857d0968a702654
Aamina Siraj <aaminasiraj23@gmail.com>
2026-08-03 02:05:59 +0530

    nested loops, patterns

---
+++ Basics/4pattern.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void){
+
+    int n;
+    
+    printf("Enter the size of the square: ");
+    scanf("%d", &n);
+    
+    for (int i = 1; i <= n; i++) {         
+        for (int j = 1; j <= i; j++) {     
+            printf("%d ",j);
+        }
+        printf("\n");                    
+    }
+} 
+
+
+
+//1
+//12
+//123
+//1234
+//12345
